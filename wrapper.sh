67cc06165e6878c1cd72d9d5bea7c8ca6f038fbe
Michael Shaw <Michaelshaw3@mail.weber.edu>
2017-02-14 20:38:00 +0000

    Wrapper file wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: wrapper.sh
+# 
+#         USAGE: ./wrapper.sh 
+# 
+#   DESCRIPTION: Wrapper to run all scripts
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Electron Group
+#  ORGANIZATION: 
+#       CREATED: 02/14/2017 20:21
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+#wrapper
+
+exit 0
+
