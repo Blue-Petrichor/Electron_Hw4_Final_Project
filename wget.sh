581dc83998ec33abfc6501d2536be0032717a26c
Blake Johnson <blakejohnson6@mail.weber.edu>
2017-02-14 13:37:54 -0700

    adding wget file

---
+++ wget.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: wget.sh
+# 
+#         USAGE: ./wget.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Blake Johnosn (), blakejohnson6@mail.weber.edu
+#  ORGANIZATION: 
+#       CREATED: 02/14/2017 13:36
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+exit 0
+
