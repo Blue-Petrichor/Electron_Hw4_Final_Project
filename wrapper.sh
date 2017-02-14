566a912f7c94676db637a2af996d522fb789ea45
Michael Shaw <Michaelshaw3@mail.weber.edu>
2017-02-14 22:35:26 +0000

    Updated wrapper.sh file with some error handling

--- wrapper.sh
+++ wrapper.sh
@@ -19,7 +19,27 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
-#wrapper
+
+#variable to store file into
+wgetFile="$1"
+
+
+# Check if file has been entered
+if [[ -z $1  ]]
+then
+	echo "No file has been entered!!!"
+	echo
+else
+	echo "Verifying file is correct..."
+	echo
+fi
+
+#verify the file is correct with either 2015 or 2016 filewhile read line; do
+if [[ $1 =~ 2015 || $1 =~ 2016  ]]  
+then 
+	echo "$wgetFile is a verified file name. "
+else
+	echo "$wgetFile is not a valid file name."
+fi
 
 exit 0
-
