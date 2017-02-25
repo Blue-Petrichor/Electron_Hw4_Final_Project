dae0d2093f4482c2e4216ebee341af093822024c
Michael Shaw <Michaelshaw3@mail.weber.edu>
2017-02-25 17:07:48 +0000

    Crontab log file for redirect to txt file

---
+++ crontab_log.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: crontab_log.sh
+# 
+#         USAGE: ./crontab_log.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Electron Group
+#  ORGANIZATION: 
+#       CREATED: 02/25/2017 17:03
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+exit 0
+
